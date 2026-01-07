a52a5badd8ba37f8bec1ea62184e212da8fc39eb
Mdjakaria53 <kjakaria53@gmail.com>
2026-01-07 15:29:14 +0300

    List element access and iterators and modifiers and operations

--- introducation_to_Data_Structures/STL_List_And_Cycle_Detection/List_Capacity.cpp
+++ introducation_to_Data_Structures/STL_List_And_Cycle_Detection/List_Capacity.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main(){
 
+list<int>l = {34,43 ,342,453,};
+// l.clear();
+
+// cout<< l.empty() ;
+cout<< l.size()<<endl ;
+l.resize(6,34);
+cout<< l.size()<<endl ;
+
 
 
 return 0;
---
+++ introducation_to_Data_Structures/STL_List_And_Cycle_Detection/List_Element_Access.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+list<int>l = {34,32,53,23,34};
+
+cout<< l.back()<<endl ;
+cout<< l.front()<<endl ;
+
+cout<< *next(l.begin(),2) ;
+
+
+
+
+return 0;
+}
