5bd2090f91daf3152c0b0bd455327b1437ec723d
Mdjakaria53 <kjakaria53@gmail.com>
2026-01-26 22:06:30 +0300

    problem solving binary tree

---
+++ introducation_to_Data_Structures/Problem Solving Binary Tree/STL_Pair.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+pair<int,int>p;
+p = make_pair(2,9);
+cout<< p.first<<endl ;
+cout<< p.second<<endl ;
+
+vector<pair<int,int>>pv = {{2,3},{4,6},{6,6}};
+
+cout<< pv[0].second ;
+
+vector<int>v;
+v.size
+
+
+
+return 0;
+}
